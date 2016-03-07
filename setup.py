9bef0c18d8439ba632fe64bf266c9d2dd343c822
Collin Delker <developer@collindelker.com>
2016-03-06 20:10:50 -0700

    Bump version to 0.2.2.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'SchemDraw',
     packages = ['SchemDraw'],
-    version = '0.2.1',
+    version = '0.2.2',
     description = 'Electrical circuit schematic drawing',
     author = 'Collin J. Delker',
     author_email = 'developer@collindelker.com',

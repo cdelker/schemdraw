222c758b70b082e4fbbcaa0d1c7adf891da01387
Collin Delker <developer@collindelker.com>
2020-01-26 17:01:49 -0700

    Updated docs config for readthedocs.

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,8 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
+templates_path = []
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -54,4 +55,8 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
+html_static_path = []
+
+master_doc = 'index'  # See https://github.com/readthedocs/readthedocs.org/issues/2569
+

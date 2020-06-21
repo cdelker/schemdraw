a8da010e4eeb0974fb02894597f547c727ef11ef
Collin Delker <developer@collindelker.com>
2020-06-21 14:41:40 -0600

    Version 0.7

--- schemdraw/__init__.py
+++ schemdraw/__init__.py
@@ -2,4 +2,4 @@ from .schemdraw import Drawing
 from .segments import Segment, SegmentCircle, SegmentArc, SegmentText, SegmentPoly, SegmentArrow
 from .transform import Transform
 
-__version__ = '0.7a2'
+__version__ = '0.7'
